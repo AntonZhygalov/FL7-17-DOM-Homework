512ca743e3d69c24ad8e0f048585a6efd26f5778
Anton <antonzhygalov@gmail.com>
2017-08-14 14:55:43 +0300

    Homework

---
+++ js/script.js
@@ -0,0 +1,339 @@
+var students = [{
+    name: 'Liudmyla',
+    lastName: 'Bashta',
+    img: 'https://www.plagiarismtoday.com/wp-content/uploads/2016/07/nyancat-385-sized.jpg',
+    coverImg: 'http://i2.kym-cdn.com/photos/images/facebook/000/243/865/8f3.jpg',
+    email: 'liudmyla.bashta@gmail.com',
+    skills: ['Javascript', 'HTML', 'CSS']
+}, {
+    name: 'Roman',
+    lastName: 'Chapkailo',
+    img: 'https://s-media-cache-ak0.pinimg.com/736x/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5--pikachu-halloween-costume-diy-halloween-costumes.jpg',
+    coverImg: 'http://fbcovershub.com/media/cover-250-beautiful-seaside-fb-cover-1388015476.jpg',
+    email: 'romafromukraine@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Khrystyna',
+    lastName: 'Dalivska',
+    img: 'https://ichef-1.bbci.co.uk/news/660/cpsprodpb/169F6/production/_91026629_gettyimages-519508400.jpg',
+    coverImg: 'https://sky.easypano.com/EPSUpload2/Pano/2017/04-12/12/636275969355928205/560_315.jpg',
+    email: 'khrystynadalivska@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Ivan',
+    lastName: 'Gnatyshyn',
+    img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+    coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+    email: 'gnatyshyn.ivan@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Andrii',
+    lastName: "Hun'ka",
+    img: 'https://cdn.pixabay.com/photo/2017/01/06/19/15/soap-bubble-1958650_960_720.jpg',
+    coverImg: 'http://i.dailymail.co.uk/i/pix/2017/01/16/20/332EE38400000578-4125738-image-a-132_1484600112489.jpg',
+    email: 'andriyggg@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Maksym',
+    lastName: 'Izmailov',
+    img: 'https://cdn.pixabay.com/photo/2016/04/17/10/38/doberman-1334497_960_720.jpg',
+    coverImg: 'https://cdn.pixabay.com/photo/2016/03/06/05/03/sunrise-1239728_960_720.jpg',
+    email: 'maksym.izmailov.lv@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Taras',
+    lastName: 'Kharkhalis',
+    img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+    coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+    email: 'taraskharkhalis97@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Khrystia',
+    lastName: 'Kondratovych',
+    img: 'https://www.webdesign.org/img_articles/21726/17.jpg',
+    coverImg: 'https://cdn.pixabay.com/photo/2016/10/13/10/37/dandelion-1737324_960_720.jpg',
+    email: 'khrustyk@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Iuliia',
+    lastName: 'Kurylo',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSljkhtuNjmEz2YeriPLPdntnTKNAwXFOAQSx1u6yAkPhYYw8-Pnw',
+    coverImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT-qepvLICH8tsGuZqbZwpO7rk5afp274Lu4bgjai8Uo30gDKifA',
+    email: 'iulia.kurylo@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Roman',
+    lastName: 'Mandziuk',
+    img: 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg',
+    coverImg: 'http://html.com/wp-content/uploads/very-large-flamingo.jpg',
+    email: 'rmandzyuk94@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Oleh',
+    lastName: 'Marko',
+    img: 'https://www.petdrugsonline.co.uk/images/page-headers/cats-master-header',
+    coverImg: 'http://wiki-carpathians.com/wp-content/uploads/2015/02/climate-carpathians.jpg',
+    email: 'olehmarko@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Anatoliy',
+    lastName: 'Mazur',
+    img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+    coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+    email: 'mail4tolik@gmail.com',
+    skills: ['JavaScript', 'CSS', 'HTML']
+}, {
+    name: 'Vitaliy',
+    lastName: 'Palyushok',
+    img: 'https://www.mammoth.com.au/res/images/mammothcloud/person-img.png',
+    coverImg: 'http://facebookcovers.piz18.com/wp-content/uploads/2012/04/geek-fb-cover.jpg',
+    email: 'xskeletons@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Stepan',
+    lastName: 'Prokopiak',
+    img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+    coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+    email: 'sprokopyak96@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Stepan',
+    lastName: 'Sendun',
+    img: 'http://i.piccy.info/i9/a25aa836358fb23a05d6e9207c976dd9/1500482900/30480/1163444/537377255ws00241_57th_annua.jpg',
+    coverImg: 'http://i.piccy.info/i9/b311de1aaff52532b361a178e8e35de4/1500482959/135850/1163444/0008540461_10.jpg',
+    email: 'steve.neeson21@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Andrii',
+    lastName: 'Soroka',
+    img: '',
+    coverImg: '',
+    email: '',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Illya',
+    lastName: 'Syniuk',
+    img: 'https://www.facebook.com/photo.php?fbid=578754465640942&set=a.317602991756092.1073741826.100005191805447&type=3&theater',
+    coverImg: 'https://www.facebook.com/photo.php?fbid=578802345636154&set=a.326403767542681.1073741828.100005191805447&type=3&theater',
+    email: 'illyasynuik@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Andrew',
+    lastName: 'Tantsiura',
+    img: 'https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_960_720.jpg',
+    coverImg: 'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
+    email: 'andrii.tans@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Liliya',
+    lastName: 'Tserkovna',
+    img: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-1/c0.17.160.160/p160x160/14725559_311214412585028_1352062715786494390_n.jpg?oh=b2cbcb3de774187b75d5253a276bc2f7&oe=59F5D47B',
+    coverImg: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/14368772_295189700854166_8626244722206545788_n.jpg?oh=02cf7516f9337bc439a42595ff893821&oe=5A051EC4',
+    email: 'lilichkaTserkovna@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}, {
+    name: 'Anton',
+    lastName: 'Zhygalov',
+    img: 'http://static.tvtropes.org/pmwiki/pub/images/Hello_Kitty_Pink_2981.jpg',
+    coverImg: 'https://thumb1.shutterstock.com/display_pic_with_logo/156640/208511908/stock-photo-arad-romania-september-hello-kitty-pattern-printed-on-cardboard-box-studio-shot-208511908.jpg',
+    email: 'antonzhygalov@gmail.com',
+    skills: ['JavaScript', 'HTML', 'CSS']
+}];
+
+
+var myContainer = document.getElementById('container')
+var myTable = document.createElement('table');
+var myThead = document.createElement('thead');
+var myTr = document.createElement('tr');
+var myTd = document.createElement('td');
+var headers = ['Student', 'Email', 'Profile picture', 'Skills', 'Controls'];
+var text = null;
+var labels = ['Name', 'Lastname', 'Email', 'Profile picture', 'Skills'];
+var inputs = ['text', 'text', 'email', 'url', 'text'];
+var buttons = ['Save button', 'Cancel button'];
+var btnType = ['button', 'reset'];
+var studentKeys = ['name', 'lastName', 'email', 'img', 'skills'];
+var formaInputs = document.getElementById('myForm');
+var myDiv = document.createElement('div');
+
+function displayForm() {
+    var myForm = document.createElement('form');
+    for (var i = 0; i < labels.length; i++) {
+        var myLabel = document.createElement('label');
+        var myInput = document.createElement('input');
+        var myBr = document.createElement('br');
+        text = document.createTextNode(labels[i]);
+        myLabel.appendChild(text);
+        myForm.appendChild(myLabel);
+        myForm.appendChild(myBr);
+        myInput.setAttribute('type', inputs[i]);
+        myInput.setAttribute('name', labels[i]);
+        myForm.appendChild(myInput);
+        myBr = document.createElement('br');
+        myForm.appendChild(myBr);
+
+    }
+    for (var i = 0; i < buttons.length; i++) {
+        var myButton = document.createElement('button');
+        myButton.setAttribute('type', btnType[i]);
+        myButton.setAttribute('name', buttons[i]);
+        myButton.innerHTML = buttons[i];
+        myButton.classList.add('btn');
+        myForm.appendChild(myButton);
+        myButton.style.margin = '10px 10px 0 0';
+    }
+    myForm.setAttribute('id', 'myForm');
+    myContainer.appendChild(myForm);
+    document.getElementsByClassName('btn')[0].addEventListener('click', function() {
+        formaInputs = document.getElementById('myForm');
+        var newStudent = {};
+        if (!formaInputs[0].value || !formaInputs[1].value || !formaInputs[3].value) {
+            alert('Name, Lastname and Profile picture cannot be empty')
+        } else if (!/^[a-zA-Z]+$/.test(formaInputs[0].value) || !/^[a-zA-Z]+$/.test(formaInputs[1].value)) {
+            alert('Name and Lastname must contain only A-z');
+        } else if (!/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(formaInputs[2].value) && formaInputs[2].value) {
+            alert('Incorrect Email')
+        } else if (!/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(formaInputs[3].value)) {
+            alert('Profile Picture must be URL');
+        } else if (!/^\s*(?:\w+\s*,\s*){0,}(?:\w+\s*)$/.test(formaInputs[4].value) && formaInputs[4].value) {
+            alert('Skills must be compound words separated by comma')
+        } else {
+            for (var i = 0; i < formaInputs.length - 2; i++) {
+                newStudent[studentKeys[i]] = formaInputs[i].value;
+            }
+        }
+        if (newStudent.hasOwnProperty('name')) {
+            students.push(newStudent);
+            document.getElementById('myTbody').remove();
+            displayStudents();
+            document.getElementById("myForm").reset();
+        }
+    });
+
+}
+
+function displayHeader() {
+    for (var i = 0; i < headers.length; i++) {
+        text = document.createTextNode(headers[i]);
+        myTd = document.createElement('td');
+        myTd.appendChild(text);
+        myTr.appendChild(myTd);
+    }
+    myThead.appendChild(myTr);
+    myDiv = document.createElement('div');
+    myDiv.classList.add('glyphicon', 'glyphicon-sort');
+    myTr.firstChild.appendChild(myDiv);
+    myTable.appendChild(myThead);
+    myThead.addEventListener('click', function(event) {
+        var target = event.target || event.srcElement;
+
+        if (target.className === 'glyphicon glyphicon-sort' || target.className === 'glyphicon glyphicon-sort-by-alphabet-alt') {
+            target.classList.remove('glyphicon-sort');
+            target.classList.remove('glyphicon-sort-by-alphabet-alt');
+            target.classList.add('glyphicon-sort-by-alphabet');
+            students.sort(function(a, b) {
+                var nameA = a.name.toUpperCase();
+                var nameB = b.name.toUpperCase();
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+            document.getElementById('myTbody').remove();
+            displayStudents();
+        } else if (target.className === 'glyphicon glyphicon-sort-by-alphabet') {
+            target.classList.remove('glyphicon-sort-by-alphabet');
+            target.classList.add('glyphicon-sort-by-alphabet-alt');
+            students.sort(function(a, b) {
+                var nameA = a.name.toUpperCase();
+                var nameB = b.name.toUpperCase();
+                if (nameA > nameB) {
+                    return -1;
+                }
+                if (nameA < nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+            document.getElementById('myTbody').remove();
+            displayStudents();
+        }
+
+    });
+}
+
+function displayStudents() {
+    var myTbody = document.createElement('tbody');
+    for (var i = 0; i < students.length; i++) {
+        myTr = document.createElement('tr');
+        // text = document.createTextNode(students[i].name + ' ' + students[i].lastName);
+        myTd = document.createElement('td');
+        // myTd.appendChild(text);
+        myTd.innerHTML = students[i].name + ' ' + students[i].lastName;
+        myTr.appendChild(myTd);
+
+        text = document.createTextNode(students[i].email);
+        myTd = document.createElement('td');
+        myTd.appendChild(text);
+        myTr.appendChild(myTd);
+
+        myTd = document.createElement('td');
+        var myImg = document.createElement('img');
+        myImg.setAttribute('src', students[i].img);
+        myTd.appendChild(myImg);
+        myImg.style.width = '100px';
+        myTr.appendChild(myTd);
+
+        text = document.createTextNode(students[i].skills);
+        myTd = document.createElement('td');
+        myTd.appendChild(text);
+        myTr.appendChild(myTd);
+
+        myTd = document.createElement('td');
+        myDiv = document.createElement('div');
+        myDiv.classList.add('glyphicon', 'glyphicon-edit');
+        myTd.appendChild(myDiv);
+        myDiv = document.createElement('div');
+        myDiv.classList.add('glyphicon', 'glyphicon-trash');
+        myTd.appendChild(myDiv);
+        myTr.appendChild(myTd);
+        myTbody.appendChild(myTr);
+    }
+    myTable.appendChild(myTbody);
+    myContainer.appendChild(myTable);
+    myTbody.setAttribute('id', 'myTbody');
+
+    myTbody.addEventListener('click', function(event) {
+        var target = event.target || event.srcElement;
+
+        if (target.className === 'glyphicon glyphicon-trash') {
+            target.parentNode.parentNode.remove();
+            for (var i = 0; i < students.length; i++) {
+                if (students[i].lastName === target.parentNode.parentNode.firstChild.innerHTML.split(' ')[1] && students[i].name === target.parentNode.parentNode.firstChild.innerHTML.split(' ')[0]) {
+                    students.splice(i, 1);
+
+                }
+            }
+        } else if (target.className === 'glyphicon glyphicon-edit') {
+            formaInputs = document.getElementById('myForm');
+            var targetChilds = target.parentNode.parentNode.childNodes;
+            formaInputs[0].value = targetChilds[0].innerHTML.split(' ')[0];
+            formaInputs[1].value = targetChilds[0].innerHTML.split(' ')[1];
+            formaInputs[2].value = targetChilds[1].innerHTML;
+            formaInputs[3].value = targetChilds[2].firstChild.getAttribute('src');
+            formaInputs[4].value = targetChilds[3].innerHTML;
+        } else if (target.getAttribute('src')) {
+            alert(target.parentNode.parentNode.firstChild.innerHTML);
+        } else alert(target.parentNode.firstChild.innerHTML);
+    });
+}
+
+displayForm();
+displayHeader()
+displayStudents();
+
+myTable.classList.add('table');
+myTable.classList.add('table-hover');
